02fc71b239e875c0f76144b9590cf2ff63ee8da1
Donatas <Donatas@DESKTOP-G1PVCUG.lan>
2018-02-22 14:24:13 +0200

    Testing

---
+++ Lab.NR1/src/Graphics_cards.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Graphics_cards
+{
+	static public List<Graphics_cards> getData()
+	{
+		List<Graphics_cards> Cards = new ArrayList<Graphics_cards>();
+		
+		
+		return Cards;		
+	}
+}
---
+++ Lab.NR1/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		List<Graphics_cards> Cards = Graphics_cards.getData();
+	}
+
+}
